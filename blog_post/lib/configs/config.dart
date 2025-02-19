a693886e2a538c8cbd6c5462c0128333682e0525
Polina Kolesnik <kolesnik.03.polina@gmail.com>
2025-02-19 12:26:53 +0700

    change architecture

--- blog_post/lib/configs/config.dart
+++
@@ -1,6 +0,0 @@
-class MyIP {
-  // static const String ipAddress = "192.168.1.82";
-  // static const String ipAddress = "192.168.64.24";
-  static const String ipAddress = "10.15.19.92";
-  // static const String ipAddress = "192.168.155.24";
-}
